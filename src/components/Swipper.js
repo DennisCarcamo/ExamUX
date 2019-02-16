a517a51fb5e2bed8cc7ed070ba0aec550858592a
DennisCarcamo <joel.carcamos@unitec.edu>
2019-02-16 00:53:52 -0600

    agregando Swipper

---
+++ src/components/Swipper.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "semantic-ui-css/semantic.min.css";
+
+class Swipper extends component{
+    render(){
+        return(
+            null
+        );
+    }
+}
+export default Cards;

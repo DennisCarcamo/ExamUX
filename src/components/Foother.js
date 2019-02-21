e4bfdc76ca2469514a0d634befa9774673511bdb
DennisCarcamo <joel.carcamos@unitec.edu>
2019-02-21 09:08:07 -0600

    Link a github

--- src/components/Foother.js
+++ src/components/Foother.js
@@ -3,23 +3,27 @@ import React, { Component } from "react";
 class Foother extends Component {
   render() {
     return (
-        <div className="ui stackable three column grid">
-        
-          <a className="column" href="https://www.facebook.com/nest/">
-            <i className="facebook f icon" />
-          </a>
-        
+      <div className="ui stackable four column grid">
+        <a className="column" href="https://www.facebook.com/nest/">
+          <i className="facebook f icon" />
+        </a>
 
-          <a className="column" href="https://twitter.com/nest">
-            <i className="twitter icon" />
-          </a>
-          <a
-            className="column"
-            href="https://www.youtube.com/channel/UCWmlRLAOpx9f1t_cVkBx8-g"
-          >
-            <i className="youtube icon" />
-          </a>
-        </div>
+        <a className="column" href="https://twitter.com/nest">
+          <i className="twitter icon" />
+        </a>
+        <a
+          className="column"
+          href="https://www.youtube.com/channel/UCWmlRLAOpx9f1t_cVkBx8-g"
+        >
+          <i className="youtube icon" />
+        </a>
+        <a
+          className="column"
+          href="https://github.com/DennisCarcamo/ExamUX.git"
+        >
+          <i className="github icon" />
+        </a>
+      </div>
     );
   }
 }
